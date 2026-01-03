16de9b3f4e26513176450c60f062bde39166d3ee
Sudheerkumar099 <tsudheerkumar09@gmail.com>
2026-01-03 17:23:22 +0530

    Summerized:[Sudheer] Summerized the unique ip addresses and http_satus_codes

--- Log-File-Analyzer/src/fake_generator.py
+++ Log-File-Analyzer/src/fake_generator.py
@@ -11,15 +11,15 @@ descriptions ={
 
 def create_fake_logs(n):
     with open("server_log.txt","w",newline="") as f:
-        f.write("Date-Time Log-Level Description IP-Address HttpCode\n")
+        # f.write("Date-Time Log-Level Description IP-Address HttpCode\n")
         fake = Faker()
-        for i in range(1,n):
+        for i in range(n):
             date_time = fake.date_time().strftime("%d-%m-25 %H:%M:%S")
             log_level = fake.random_element(log_levels)
             description = fake.random_element(descriptions[log_level])
             ip_address = fake.ipv4()
-            http_code = fake.http_status_code()
+            http_code = fake.random_element(["200","201","400","401","403","404","500","502","503"])
             f.write(f"{date_time} {log_level} {description} {ip_address} {http_code}\n")
         print(f"{n} log entries created")
         
-create_fake_logs(10)
+create_fake_logs(20)
--- Log-File-Analyzer/src/summarize.py
+++ Log-File-Analyzer/src/summarize.py
@@ -1,14 +1,43 @@
 import re
 unique_ip = set()
 ip_pattern = r"\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}"
-ip_lst = {}
-with open("server_log.txt", "r") as f:
-    logs = f.readlines()
-    for line in logs[1:]:
-        ip_lst = re.findall(ip_pattern, line)
-        if ip_lst:
-            print(ip_lst)
-        
-        
+http_pattern = r"\d{3}$"
+ip_lst = []
+def summarize_unique_ip():
+    with open("server_log.txt", "r") as f:
+        with open('log_summary.txt', 'w') as s:
+            s.write("Unique IP Addresses:\n")
+            logs = f.readlines()
+            ip_lst=[]
+            ip_set = set()
+            for line in logs:
+                ip_lst.extend(re.findall(ip_pattern, line))
+            if ip_lst:
+                ip_set = set(ip_lst)
+            for ip in ip_set:
+                count = 0
+                for i in ip_lst:
+                    if i == ip:
+                        count += 1
+                s.write(f"{ip} ")
+                s.write(f"     :  {count} \n")
+    print("server Log summarized")
 
-
+def summerize_http_status():
+    with open("server_log.txt", "r") as f:
+        with open('log_summary.txt', 'a') as s:
+            s.write("HTTP Status Codes:\n")
+            logs = f.readlines()
+            http_lst=[]
+            for line in logs:
+                http_lst.extend(re.findall(http_pattern, line))
+            http_set = set(http_lst)
+            for code in http_set:
+                count = 0
+                for i in http_lst:
+                    if i == code:
+                        count += 1
+                s.write(f"{code} : {count} \n")
+    print("server Log summarized")
+summarize_unique_ip()
+summerize_http_status()
