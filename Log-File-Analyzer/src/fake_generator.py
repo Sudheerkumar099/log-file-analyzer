6589dcaa911cb40a4df668fa829dcebd0d3bea99
Sudheerkumar099 <tsudheerkumar09@gmail.com>
2026-01-03 17:50:01 +0530

    CustomFakers: [Sudheer] Added Custom fakers using providers

--- Log-File-Analyzer/src/fake_generator.py
+++ Log-File-Analyzer/src/fake_generator.py
@@ -1,24 +1,31 @@
-from faker import Faker 
+from faker import Faker
+from faker.providers import BaseProvider
+class LogProvider(BaseProvider):
+    def LogLevel(self):
+        levels = ["INFO", "WARNING", "ERROR", "CRITICAL"]
+        return self.random_element(levels) 
+    def description(self):
+        desc = ["User logged in successfully","Request processed successfully",
+                "Password attempt limit nearing","Deprecated API used",
+                "Database connection failed","Invalid input received",
+                "System out of memory","Service crashed unexpectedly"]
+        return self.random_element(desc)
+    def http_code(self):
+        codes = ["200","201","400","401","403","404","500","502","503"]
+        return self.random_element(codes)
+fake = Faker()
+fake.add_provider(LogProvider)
 
-log_levels = ["INFO", "WARNING", "ERROR", "CRITICAL"]
-
-descriptions ={
-    "INFO": ["User logged in successfully","Request processed successfully"],
-    "WARNING": ["Password attempt limit nearing","Deprecated API used"],
-    "ERROR": ["Database connection failed","Invalid input received"],
-    "CRITICAL": ["System out of memory","Service crashed unexpectedly"]
-    }
 
 def create_fake_logs(n):
     with open("server_log.txt","w",newline="") as f:
         # f.write("Date-Time Log-Level Description IP-Address HttpCode\n")
-        fake = Faker()
         for i in range(n):
             date_time = fake.date_time().strftime("%d-%m-25 %H:%M:%S")
-            log_level = fake.random_element(log_levels)
-            description = fake.random_element(descriptions[log_level])
+            log_level = fake.LogLevel()
+            description = fake.description()
             ip_address = fake.ipv4()
-            http_code = fake.random_element(["200","201","400","401","403","404","500","502","503"])
+            http_code = fake.http_code()
             f.write(f"{date_time} {log_level} {description} {ip_address} {http_code}\n")
         print(f"{n} log entries created")
         
